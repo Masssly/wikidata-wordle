f9ca697853957b8fedcf9c94accf05471328745b
Mohammed Sadat Abdulai <mohammed.sadat.abdulai@gmail.com>
2025-11-30 21:58:47 +0100

    Implement QueryBuilder class for query construction
    Added QueryBuilder class with methods to build queries for word types and lexeme details.

---
+++ src/modules/data/QueryBuilder.js
@@ -0,0 +1,31 @@
+import { BASE_QUERY_TEMPLATES } from './queries/baseQueries.js';
+import { GAME_CONSTANTS } from '../config/constants.js';
+
+export class QueryBuilder {
+    static buildQuery(language, wordType = 'nouns', options = {}) {
+        const template = BASE_QUERY_TEMPLATES[wordType.toUpperCase()];
+        
+        if (!template) {
+            throw new Error(`No query template found for word type: ${wordType}`);
+        }
+
+        const defaults = {
+            LIMIT: options.limit || 50,
+            MIN_LENGTH: options.minLength || GAME_CONSTANTS.WORD_LENGTH.MIN,
+            MAX_LENGTH: options.maxLength || GAME_CONSTANTS.WORD_LENGTH.MAX
+        };
+
+        return template
+            .replace('{{LANGUAGE_QID}}', language.qid)
+            .replace('{{LANGUAGE_CODE}}', language.code)
+            .replace('{{LIMIT}}', defaults.LIMIT)
+            .replace('{{MIN_LENGTH}}', defaults.MIN_LENGTH)
+            .replace('{{MAX_LENGTH}}', defaults.MAX_LENGTH);
+    }
+
+    static buildLexemeDetailsQuery(lexemeQid, languageCode) {
+        return BASE_QUERY_TEMPLATES.LEXEME_DETAILS
+            .replace('{{LEXEME_QID}}', lexemeQid)
+            .replace('{{LANGUAGE_CODE}}', languageCode);
+    }
+}
