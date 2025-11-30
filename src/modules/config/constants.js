059d132f9087d1b00f22929e9f743c76d752c3c3
Mohammed Sadat Abdulai <mohammed.sadat.abdulai@gmail.com>
2025-11-30 22:49:12 +0100

    Refactor GAME_CONSTANTS into individual exports

--- src/modules/config/constants.js
+++ src/modules/config/constants.js
@@ -1,19 +1,31 @@
+// src/modules/config/constants.js
+// Using simple exports to avoid circular dependencies
+
+export const MAX_ATTEMPTS = 6;
+
+export const WORD_LENGTH = {
+    MIN: 3,
+    MAX: 12
+};
+
+export const HINTS = {
+    GRAMMATICAL_FEATURES: 'grammatical_features',
+    DEFINITION: 'definition',
+    IMAGE: 'image',
+    TRANSLATIONS: 'translations',
+    PRONUNCIATION: 'pronunciation'
+};
+
+export const DIFFICULTY_LEVELS = {
+    EASY: ['grammatical_features', 'definition'],
+    MEDIUM: ['image', 'translations'],
+    HARD: ['pronunciation']
+};
+
+// If you need a combined object, create it after all individual exports
 export const GAME_CONSTANTS = {
-    MAX_ATTEMPTS: 6,
-    WORD_LENGTH: {
-        MIN: 3,
-        MAX: 12
-    },
-    HINTS: {
-        GRAMMATICAL_FEATURES: 'grammatical_features',
-        DEFINITION: 'definition',
-        IMAGE: 'image',
-        TRANSLATIONS: 'translations',
-        PRONUNCIATION: 'pronunciation'
-    },
-    DIFFICULTY_LEVELS: {
-        EASY: [GAME_CONSTANTS.HINTS.GRAMMATICAL_FEATURES, GAME_CONSTANTS.HINTS.DEFINITION],
-        MEDIUM: [GAME_CONSTANTS.HINTS.IMAGE, GAME_CONSTANTS.HINTS.TRANSLATIONS],
-        HARD: [GAME_CONSTANTS.HINTS.PRONUNCIATION]
-    }
+    MAX_ATTEMPTS,
+    WORD_LENGTH,
+    HINTS,
+    DIFFICULTY_LEVELS
 };
